84bfa0b4138c8e550e3c2a77ca8e53cd91bdc28d
rodrigolf080 <rodrigolf080@gmail.com>
2021-03-22 20:12:27 +0000

    makefile partial;main essential files; src hold the bins;

---
+++ src/macros.h
@@ -0,0 +1,10 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+// -- Helper Functions
+// :: Clear Screen Function
+#define clear() printf("\033[2J\033[1;1H");
+// :: Initial Program Greeting
+void greeting();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "macros.h"
+int main(int argc, char **argv)
+{
+	greeting();
+	return 0;
+}
